e002e4895fac65a12a75f85ae041a1e98e52aebe
duks <duks@robooot.com>
2017-02-07 18:36:00 +0800

    Initial Commit

---
+++ FMDBManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBManager
+//
+//  Created by aDu on 2017/2/7.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
